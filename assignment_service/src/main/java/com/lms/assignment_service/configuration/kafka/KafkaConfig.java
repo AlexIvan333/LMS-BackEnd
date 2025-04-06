e44f091fc63e9c66fc2f33247fecbdd47102d2cd
AlexIvan333 <146068023+AlexIvan333@users.noreply.github.com>
2025-04-06 15:23:23 +0200

    make kafka run in all services without errors

--- assignment_service/src/main/java/com/lms/assignment_service/configuration/kafka/KafkaConfig.java
+++ assignment_service/src/main/java/com/lms/assignment_service/configuration/kafka/KafkaConfig.java
@@ -21,7 +21,7 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "host.docker.internal:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
@@ -38,7 +38,7 @@ public class KafkaConfig {
         deserializer.addTrustedPackages("*");
 
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "host.docker.internal:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "lms-group");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
--- auth_service/src/main/java/com/lms/auth_service/entities/relational/InstructorEntity.java
+++ auth_service/src/main/java/com/lms/auth_service/entities/relational/InstructorEntity.java
@@ -5,9 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-
+@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
